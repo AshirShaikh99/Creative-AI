c33a9b4d229bd980528e3c2b618d5b2a299eae3f
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-20 21:02:54 +0500

    Constants

---
+++ lib/constants/appwrite.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseID = "6468ee5de3adb3b14610";
+  static const String projectID = "6468cdedbb92d7d0ab21";
+  static const String endPoint = "http://localhost:80/v1";
+}
