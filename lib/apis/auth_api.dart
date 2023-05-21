1caef224e21f472cd18dae4f546bd18dcca4fb56
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-21 23:29:19 +0500

    Update auth_api.dart

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -7,5 +7,23 @@ abstract class IAuthAPI {
   // IAuthAPI is an interface for AuthAPI class to implement its methods //
 
   FutureEither<model.User> signUp(
-      {required String emailID, required String password});
+      {required String emailID, required String password}); // signUp method //
+}
+
+class AuthAPI implements IAuthAPI {
+  Account _account; // Account is a class from Appwrite SDK //
+
+  AuthAPI({required Account account}) : _account = account; // Constructor //
+
+  @override // Overriding the signUp method from IAuthAPI interface //
+  FutureEither<model.User> signUp(
+      {required String emailID, required String password}) async {
+    try {
+      _account.create(
+        userId: 'unique()',
+        email: emailID,
+        password: password,
+      );
+    } catch (e) {}
+  }
 }
