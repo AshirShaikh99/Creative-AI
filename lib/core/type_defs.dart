2e3d53aeb89cf724e5ab776a049b6a5167672529
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-21 22:53:38 +0500

    Defining types

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -2,3 +2,4 @@ import 'package:fpdart/fpdart.dart';
 import 'package:social_networking/core/failure.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureEitherVoid = FutureEither<void>;
