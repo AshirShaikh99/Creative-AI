04ee1981869fddaf3a3ee92385ee75d903ded4b7
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-21 23:05:32 +0500

    Setting up models for Auth

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,16 +1,11 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:social_networking/core/core.dart';
 
 abstract class IAuthAPI {
   // IAuthAPI is an interface for AuthAPI class to implement its methods //
 
-  Future<Either<String, Account>> signUp();
-}
-
-class AuthAPI implements IAuthAPI {
-  @override
-  Future<Either<String, Account>> signUp() {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
+  FutureEither<model.User> signUp(
+      {required String emailID, required String password});
 }
