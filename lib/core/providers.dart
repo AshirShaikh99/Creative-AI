b5a2a625f393cc2e93a21619a7f763e1941d93f7
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-21 23:58:16 +0500

    Adding new provider for the Account

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -2,12 +2,20 @@ import 'package:riverpod/riverpod.dart';
 import 'package:appwrite/appwrite.dart';
 import 'package:social_networking/constants/constants.dart';
 
-final appWriteClientProvide = Provider(
+final appWriteClientProvider = Provider(
   // Provider is a class from Riverpod //
   (ref) {
     Client client = Client(); // Client is a class from Appwrite SDK //
-    return client.setEndpoint(AppWriteConstants.endPoint).setProject(
-        AppWriteConstants
-            .projectID); // setEndpoint and setProject are methods from Client class //
+    return client
+        .setEndpoint(AppWriteConstants.endPoint)
+        .setProject(AppWriteConstants.projectID)
+        .setSelfSigned(status: true); // Setting up Appwrite client //
+  },
+);
+
+final appWriteAccountProvider = Provider(
+  (ref) {
+    final client = ref.watch(appWriteClientProvider); // Watching the client //
+    return Account(client); // Account is a class from Appwrite SDK //
   },
 );
