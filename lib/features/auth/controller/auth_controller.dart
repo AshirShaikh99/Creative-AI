47c6ab216f40fc817c55c984192624b9308770ca
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-05-22 00:59:13 +0500

    Setting up auth controller

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,28 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:social_networking/apis/auth_api.dart';
+import 'package:social_networking/core/utils.dart';
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI; // AuthAPI is a class from AuthAPI.dart //
+  AuthController(AuthAPI authAPI)
+      : _authAPI = authAPI,
+        super(false); // Constructor //
+
+  // If it is loading to get the final response from the Backend //
+
+  void signUp({
+    required String emailID,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final response = await _authAPI.signUp(
+        emailID: emailID,
+        password: password); // signUp method from AuthAPI class //
+    response.fold(
+        (l) => showSnackBar(context, l.message), (r) => print(r.name));
+  } // signUp method //
+}
