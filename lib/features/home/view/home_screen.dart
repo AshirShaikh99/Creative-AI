b8617a3559d7928cb940fe53359e4c05c87a66a4
Ashir Shaikh <99293735+AshirShaikh99@users.noreply.github.com>
2023-06-01 20:28:26 +0500

    Enhancing UI

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -23,6 +23,12 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   // get the user data from the provider
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -34,91 +40,97 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       body: Padding(
         padding: const EdgeInsets.only(top: 30),
         child: Center(
-          child: Column(
-            children: [
-              const Text(
-                "Hi Ashir",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  "Hi Ashir",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                margin: const EdgeInsets.symmetric(horizontal: 20)
-                    .copyWith(top: 30),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(20)
-                      .copyWith(topLeft: Radius.zero, topRight: Radius.zero),
+                const SizedBox(
+                  height: 5,
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  child: Text(
-                    "What task can I do for you today?",
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  margin: const EdgeInsets.symmetric(horizontal: 20)
+                      .copyWith(top: 30),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(20)
+                        .copyWith(topLeft: Radius.zero, topRight: Radius.zero),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      "What task can I do for you today?",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'serif',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    left: 22,
+                  ),
+                  child: const Text(
+                    "Explore Features",
                     style: TextStyle(
                       fontSize: 25,
                       fontFamily: 'serif',
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.only(
-                  top: 10,
-                  left: 22,
-                ),
-                child: const Text(
-                  "Explore Features",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'serif',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: const [
+                      FeatureContainer(
+                        color: Colors.lightGreenAccent,
+                        headingText: "My Instructor",
+                        subHeadingText: "Get your own instructor",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FeatureContainer(
+                        color: Colors.lightGreenAccent,
+                        headingText: "My Instructor",
+                        subHeadingText: "Get your own instructor",
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FeatureContainer(
+                        color: Colors.lightGreenAccent,
+                        headingText: "My Instructor",
+                        subHeadingText: "Get your own instructor",
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: const [
-                    FeatureContainer(
-                      color: Colors.lightGreenAccent,
-                      headingText: "My Instructor",
-                      subHeadingText: "Get your own instructor",
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    FeatureContainer(
-                      color: Colors.lightGreenAccent,
-                      headingText: "My Instructor",
-                      subHeadingText: "Get your own instructor",
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    FeatureContainer(
-                      color: Colors.lightGreenAccent,
-                      headingText: "My Instructor",
-                      subHeadingText: "Get your own instructor",
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.mic),
+        onPressed: () {},
       ),
     );
   }
